bf4d8bf3d5276b0cd64a110ff6236008950fb74b
bardia <bardia.mojra@gmail.com>
2025-08-27 11:06:08 -0500

    Updated with gitexpress on 2025-08-27.

--- dev_py/gen_points.py
+++ dev_py/gen_points.py
@@ -6,7 +6,7 @@ import pandas as pd
 import plotly.express as px
 
 np.random.seed(19680801) # Fixing random state for reproducibility
-numPoints = 100000
+numPoints = 1000
 r = 100
 x_max = r
 x_min = 0
@@ -19,12 +19,13 @@ Xs = list()
 Ys = list()
 Zs = list()
 
+
 for i in range(numPoints):
     x = np.random.uniform(x_min, x_max)
     y_max = math.sqrt( r**2 - x**2 )
     y_min = -y_max
     y = np.random.uniform(y_min, y_max)
-    z_max = math.sqrt( r**2 - y**2 )
+    z_max = math.sqrt( r**2 - y**2 - x**2 )
     z_min = -z_max
     z = np.random.uniform(z_min, z_max)
 
